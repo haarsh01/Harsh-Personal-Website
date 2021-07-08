4e64d7bd7d7263df67f0d9467050220c3a4ef7f5
haarsh01 <harshkaushik717@gmail.com>
2021-07-08 22:49:32 +0530

    Adding Folders

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+// Output 10
+// Output 0
+
